9f65bb806fa326c43ae2dc9a1fcdb03ff60cdac3
Lohith4 <allalohith4@gmail.com>
2022-05-18 14:35:15 +0530

    change2

--- service_a/service_a_app.py
+++ service_a/service_a_app.py
@@ -6,4 +6,4 @@ app = Flask(__name__)
 def hello_world():
     service_c_url = 'http://service-c/service_c'
     response = requests.get(service_c_url).text
-    return 'Welcome to service_a: ---> ' + response + "sample change"
+    return 'Welcome to service: ---> ' + response + "sample change"
